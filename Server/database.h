c0038de156a5c6a0a36402ddf4acefe0ea63ca00
Harris Ibrahimi <98238342+SUP3RBOB@users.noreply.github.com>
2025-03-10 18:30:52 -0400

    Commented header file for database

--- Server/database.h
+++ Server/database.h
@@ -4,6 +4,7 @@
 #include <QSqlDatabase>
 #include <QSqlQuery>
 
+/// Contains all functionality required for usage of database.
 class Database
 {
 private:
@@ -11,12 +12,17 @@ private:
     QSqlQuery querySingle;
 
 public:
+    /// Connects to the MySQL database.
     Database();
     ~Database();
 
+    /// Runs a query with the specified string.
     bool Query(const QString& query);
+
+    /// Runs a query with the specified query object.
     bool Query(QSqlQuery& query);
 
+    /// Returns if an account exists in the database.
     bool AccountExists(const QString& email);
 };
 
